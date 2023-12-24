0eda4d069e1619a4f35c7828dbd9d2d689421c0e
seaniod <sean@odonoghuelab.org>
2023-12-24 14:32:39 +1100

    print stuff

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 'use strict';
 
-// Here's a vanilla HTTP app to start,
-// but feel free to replace it with Express, Koa, etc
-var app = function(req, res) {
+// Original app function
+var originalApp = function(req, res) {
     res.end('Hello, Encrypted World!');
+};
+
+// Wrapper function to log requests and then call the original app function
+var app = function(req, res) {
+    // Log the request details
+    console.log(`Received request: ${req.method} ${req.url}`);
+    console.log(`Headers: ${JSON.stringify(req.headers)}`);
+
+    // Call the original app function
+    originalApp(req, res);
 };
 
 module.exports = app;
