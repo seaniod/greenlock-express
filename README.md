5524ebd3e66844d50aa7045e902c1e98900896c7
seaniod <sean@odonoghuelab.org>
2023-12-26 18:42:01 +1100

    step2

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# documentation
+* [main](https://git.rootprojects.org/root/greenlock-express.js/src/branch/master)
+* [using non-standard ports](https://git.rootprojects.org/root/greenlock-express.js/src/branch/master/examples/http/server.js)
+* [videos](https://www.youtube.com/watch?v=e8vaR4CEZ5s&list=PLZaEVINf2Bq_lrS-OOzTUJB4q3HxarlXk&index=4)
+
+
 # enable nodejs to use port 80
 ``sudo setcap 'cap_net_bind_service=+ep' $(which node)'``
 
---
+++ server3.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var app = require('./app.js');
+
+function approveDomains(opts, certs, callback) {
+    if(!/^(www\.)?test\.minardo\.org$/) {
+        callback(new Error(`no config for '${opts.domain}'`));
+        return;
+    }
+    opts.email = 'contact@minardo.org';
+    opts.agreeTos = true;
+    opts.domains = ['minardo.org', 'www.minardo.org'];
+    callback(null, {options: opts, certs: certs});
+}
+
+var staging = 'https://acme-staging-v02.api.letsencrypt.org/directory';
+var live = 'https://acme-v02.api.letsencrypt.org/directory';
+
+var greenlock = require('greenlock-express').init({
+    packageRoot: __dirname,
+    maintainerEmail: "contact@minardo.org",
+    configDir: './certificates',
+    cluster: false,
+    server: staging, // live
+    approveDomains: function(opts, certs, cb) {
+        approveDomains(opts, certs, cb)
+    }
+});
+
+// Get & update certificates automatically
+// var server = greenlock.listen(80, 443); 
+var server = greenlock.serve(app); 
